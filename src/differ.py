f59cf3fb64af0e74a40ee912f450c9ada71d1bce
Eric Chandler <ericmchandler@gmail.com>
2025-11-03 12:01:55 -0600

    make diff idempotent

--- src/differ.py
+++ src/differ.py
@@ -2,7 +2,7 @@ import logging
 import difflib
 from itertools import pairwise
 import json
-from src.blob_utils import get_blob_service_client, parse_blob_path, load_json_blob, upload_json_blob
+from src.blob_utils import get_blob_service_client, parse_blob_path, load_json_blob, upload_json_blob, check_blob
 from src.log_utils import setup_logger
 from src.docchunk import DocChunk
 from src.stages import Stage
@@ -14,27 +14,38 @@ def diff_batch() -> None:
     for company, policies in directory.items():
         for policy, snaps in policies.items():
             pairs = pairwise(sorted(snaps))
-            for pair in pairs:
-                output = _diff_files(pair[0], pair[1])
-                outpath = parse_blob_path(pair[0])
-                outname = f"{Stage.DIFF.value}{outpath.company}/{outpath.policy}/{outpath.timestamp}.json"
+            manifest = _get_manifest(company, policy)
+            for before, after in pairs:
+                outname = f"{Stage.DIFF.value}/{company}/{policy}/{after}.json"
+                if after in manifest and manifest[after] == before:
+                    logger.debug(f"Diff already computed for {outname}")
+                    continue
+                logger.debug(f"Difffing {company}/{policy} : {before} <-> {after}")
+                manifest[after] = before
+                output = _diff_files(company, policy, before, after)
                 upload_json_blob(output, 'documents', outname)
+                _store_manifest(manifest, company, policy)
 
 
-def diff_single(input_name: str):
-    directory = _list_container()
-    parts = parse_blob_path(input_name)
-    policies = directory[parts.company]
-    snaps = sorted(policies[parts.policy])
-    idx = snaps.index(input_name)
-    if idx >= 1:
-        other = snaps[idx - 1]
-        output = _diff_files(input_name, other)
-        outname = f"{Stage.DIFF.value}{parts.company}/{parts.policy}/{parts.timestamp}.json"
-        upload_json_blob(output, 'documents', outname)
+def _get_manifest(company, policy):
+    """Retrieve list of computed diffs (and reference points)."""
+    manifest_name = f"{Stage.DIFF.value}/{company}/{policy}/manifest.json"
+    if check_blob('documents', manifest_name):
+        return load_json_blob('documents', manifest_name)
+    else:
+        return {}
 
 
-def _diff_files(filenamea: str, filenameb: str) -> str:
+def _store_manifest(data, company, policy):
+    """Upload list of computed diffs (and reference points)."""
+    manifest_name = f"{Stage.DIFF.value}/{company}/{policy}/manifest.json"
+    return upload_json_blob(data, 'documents', manifest_name)
+
+
+def _diff_files(company, policy, before, after) -> str:
+    """Compute difference between two DocChunk files (parsed html lines)."""
+    filenamea = f"{Stage.DOCCHUNK.value}/{company}/{policy}/{before}.json"
+    filenameb = f"{Stage.DOCCHUNK.value}/{company}/{policy}/{after}.json"
     doca = load_json_blob('documents', filenamea)
     docb = load_json_blob('documents', filenameb)
     txta = [DocChunk.from_str(x).text for x in doca]
@@ -47,6 +58,7 @@ def _diff_files(filenamea: str, filenameb: str) -> str:
     
 
 def _diff_sequence(a, b): 
+    """Helper function to diff line-based files."""
     matcher = difflib.SequenceMatcher(lambda x: x.isspace(), a, b)
     for tag, i1, i2, j1, j2 in matcher.get_opcodes():
         matcher = difflib.SequenceMatcher(lambda x: x.isspace(), a[i1:i2], b[j1:j2])
@@ -55,7 +67,8 @@ def _diff_sequence(a, b):
                    sim=matcher.ratio())
 
 
-def _list_container():
+def _list_container() -> dict:
+    """Represent container as dictionary."""
     client = get_blob_service_client()
     container = client.get_container_client('documents')
     directory = {}
@@ -63,5 +76,6 @@ def _list_container():
         parts = parse_blob_path(name)
         policies = directory.setdefault(parts.company, {})
         snaps = policies.setdefault(parts.policy, [])
-        snaps.append(name)
+        if parts.timestamp != "manifest":
+            snaps.append(parts.timestamp)
     return directory
